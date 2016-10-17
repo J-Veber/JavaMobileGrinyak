aa362e73978079ee78de27bd21d1a65646a009d7
J-Veber <kovalsky.corp@gmail.com>
2016-10-17 20:12:34 +1000

    Нарисована страничка для контрагента

--- app/src/main/java/com/example/veiber/testmobilegrinyak/AddNewContacter.java
+++ app/src/main/java/com/example/veiber/testmobilegrinyak/AddNewContacter.java
@@ -1,7 +1,9 @@
 package com.example.veiber.testmobilegrinyak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddNewContacter extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class AddNewContacter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_contacter);
+    }
+    public void cancel_action(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void addNewItemTableContractors(View view) {
+
     }
 }
